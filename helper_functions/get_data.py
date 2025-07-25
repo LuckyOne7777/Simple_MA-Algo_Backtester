a229343af45c4c2a2dec3469032b9ab423f800c1
NAthan <nathanbsmith729@gmail.com>
2025-07-25 08:46:49 -0500

    Support Alpaca data downloading and file usage

--- helper_functions/get_data.py
+++ helper_functions/get_data.py
@@ -15,17 +15,47 @@ class Get_Historical_Data:
         secret_api_key = os.getenv("ALPACA_SECRET_KEY")
         client = StockHistoricalDataClient(api_key, secret_api_key)
         ticker = input("What is the ticker symbol? ").upper()
+        root = os.path.expanduser("~/Simple Algorithm Backtester")
+        folder = os.path.join(root, "data")
+        file = f"{ticker} Alpaca Data.parquet"
+        print(file)
+        file_path = os.path.join(folder, file) # broken
+        file_path = f"/Users/natha/Simple Algorithm Backtester/data/{file}"
+        print(file_path)
+        if os.path.exists(file_path):
+            print(f"Grabbing existing data from {file}")
+            max_timeframe = pd.read_parquet(file_path)
+            if max_timeframe.index.name != "timestamp":
+                if "timestamp" in max_timeframe.columns:
+                    max_timeframe.set_index("timestamp", inplace=True)
+            print(max_timeframe)
+        else:
+            print("No existing file found. Downloading data...")
+            try:
+                max_timeframe = StockBarsRequest(
+                    symbol_or_symbols=[ticker],
+                    timeframe=TimeFrame.Day,
+                    start=datetime(1999, 1, 1),
+                    end=datetime.now(),
+                    )
+                max_timeframe = client.get_stock_bars(max_timeframe).df
+                if max_timeframe.index.name != "timestamp":
+                    if "timestamp" in max_timeframe.columns:
+                        max_timeframe.set_index("timestamp", inplace=True)
+                print(max_timeframe)
+                if max_timeframe.empty or len(max_timeframe) < 200:
+                    raise KeyError("Downloaded data was empty.")
+                if isinstance(max_timeframe.columns, pd.MultiIndex):
+                        max_timeframe.columns = max_timeframe.columns.get_level_values(0)
+                if isinstance(max_timeframe.index, pd.MultiIndex):
+                    max_timeframe.reset_index(inplace=True)
+                    max_timeframe.set_index("timestamp", inplace=True)
 
-        max_timeframe = StockBarsRequest(
-            symbol_or_symbols=[ticker],
-            timeframe=TimeFrame.Day,
-            start=datetime(1999, 1, 1),
-            end=datetime.now(),
-        )
-
-        bars = client.get_stock_bars(max_timeframe)
-        first_date = bars[ticker][0].timestamp
-        last_date = datetime.now()
+                max_timeframe.to_parquet(file_path)
+            except Exception as e:
+                    raise ConnectionError(f"connection error likely. Error: {e}")
+        first_date = max_timeframe.index[0]
+        last_date = max_timeframe.index[-1]   
 
         user_time_preference = 0
         while user_time_preference not in ["1", "2"]:
@@ -44,19 +74,14 @@ Enter 1 or 2: """)
             end_month = int(input("What end month should it stop? (month number) "))
             end_day = int(input("What end day should it stop? (day number) "))
 
-            request_params = StockBarsRequest(
-                symbol_or_symbols=[ticker],
-                timeframe=TimeFrame.Day,
-                start=datetime(start_year, start_month, start_day),
-                end=datetime(end_year, end_month, end_day),
-            )
-            bars = client.get_stock_bars(request_params)
+            start_date = f"{start_year}-{start_month}-{start_day}"
+            end_date = f"{end_year}-{end_month}-{end_day}"
+        # fix all this
+            data = max_timeframe[(max_timeframe.index >= start_date) & (max_timeframe.index <= end_date)].reset_index()
+        else:
+            data = max_timeframe
 
-        data = bars.df.reset_index()
-        data['date'] = pd.to_datetime(data['timestamp']).dt.date
-
-        if isinstance(data.columns, pd.MultiIndex):
-            data.columns = data.columns.get_level_values(0)
+        data = data.reset_index()
 
         if data.empty or len(data) < 200:
             raise KeyError(f"No sufficient data for {ticker}")
@@ -83,6 +108,8 @@ Enter 1 or 2: """)
                 print("No existing file found. Downloading data...")
                 try:
                     max_timeframe = yf.download(ticker, period="max", auto_adjust=True)
+                    if isinstance(max_timeframe.columns, pd.MultiIndex):
+                        max_timeframe.columns = max_timeframe.columns.get_level_values(0)
                 except Exception as e:
                     raise ConnectionError(f"connection error likely. Error: {e}")
                 max_timeframe.to_parquet(file_path)
